2fb48f08e8f7e44b4bd2824520c1ecf7b91ea4a9
eggcelsior <makutaverok@gmail.com>
2022-03-16 12:51:31 -0700

    Fixed weapon movement

--- SmallRoguelike/Assets/Scripts/PlayerController.cs
+++ SmallRoguelike/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,7 @@ public class PlayerController : MonoBehaviour
     private Vector2 input = new Vector2();
     public Animator anim;
     public Transform gunArm;
+    public Transform weaponTransform;
 
     [Header("Ability Enablers")]
     //Dash Things
@@ -53,7 +54,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerMove();
-        WeaponMovement();
+        //WeaponRotating();
+        WeaponPointing();
     }
     private void PlayerMove()
     {
@@ -62,14 +64,14 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
             input.Normalize();
-            if(input.x < 0)
+            /*if(input.x < 0)
             {
                 rotation.y = 180;
             }
             if(input.x > 0)
             {
                 rotation.y = 0;
-            }
+            }*/
             transform.localRotation = rotation;
             rb.velocity = input * speed;
             //Debug.Log(input);
@@ -87,7 +89,7 @@ public class PlayerController : MonoBehaviour
         }
     }
     
-    private void WeaponMovement()
+    private void WeaponRotating()
     {
         Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0f;
@@ -97,6 +99,24 @@ public class PlayerController : MonoBehaviour
         Vector3 offset = mousePos - centerPoint;
 
         weaponTest.transform.position = centerPoint + Vector3.ClampMagnitude(offset, distance);
+    }
+    private void WeaponPointing()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = cam.WorldToScreenPoint(transform.localPosition);
+        int modifier = -45;
+        Vector3 difference = cam.ScreenToWorldPoint(Input.mousePosition) - weaponTransform.transform.position;
+        difference.Normalize();
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        weaponTransform.transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + modifier);
+        if (mousePos.x < screenPoint.x)
+        {
+            rotation.y = 180;
+        }
+        if (mousePos.x > screenPoint.x)
+        {
+            rotation.y = 0;
+        }
     }
 
     private void Dashing()
@@ -130,5 +150,12 @@ public class PlayerController : MonoBehaviour
             dashTimer -= Time.deltaTime;
         }
     }
-
+    //Yoinked from https://forum.unity.com/threads/clampmagnitude-why-no-minimum.388488/
+    public static Vector3 ClampMagnitude(Vector3 v, float max, float min)
+    {
+        double sm = v.sqrMagnitude;
+        if (sm > (double)max * (double)max) return v.normalized * max;
+        else if (sm < (double)min * (double)min) return v.normalized * min;
+        return v;
+    }
 }
